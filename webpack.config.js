ce33d8c65e6701e8162218e74cfd9744d79b3eab
siddharthapaul001 <siddharthapaul@live.com>
2020-04-01 13:54:59 +0530

    build script updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,21 @@ const path = require('path');
 const webpack = require('webpack');
 
 let libraryName = 'DeTransform',
-	fileName = 'detransform.js',
-	mode = 'development';
+	fileName = 'detransform',
+  mode = process.argv[3];
+
+if (mode === 'production') {
+  fileName += '.min.js';
+} else {
+  fileName += '.js';
+  mode = 'development';
+}
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: fileName,
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/build'),
     library: libraryName,
     libraryTarget: 'umd'
   },
